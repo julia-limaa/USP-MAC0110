6b07a718e9099ce410d931c3413a6440e58dd950
julia-limaa <83968387+julia-limaa@users.noreply.github.com>
2021-05-21 20:13:12 -0300

    Adicionei ep01

---
+++ 03 ep01.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------
+# LEIA E PREENCHA O CABEÇALHO 
+# NÃO ALTERE OS NOMES DAS FUNÇÕES, MÉTODOS E ATRIBUTOS
+# NÃO APAGUE OS DOCSTRINGS
+#------------------------------------------------------------------
+     
+'''
+    Nome: Júlia de Lima Oliveira
+    NUSP: 12567082
+
+    Ao preencher esse cabeçalho com o meu nome e o meu número USP,
+    declaro que todas as partes originais desse exercício programa (EP)
+    foram desenvolvidas e implementadas por mim e que portanto não 
+    constituem desonestidade acadêmica ou plágio.
+    Declaro também que sou responsável por todas as cópias desse
+    programa e que não distribui ou facilitei a sua distribuição.
+    Estou ciente que os casos de plágio e desonestidade acadêmica
+    serão tratados segundo os critérios divulgados na página da 
+    disciplina.
+    Entendo que EPs sem assinatura devem receber nota zero e, ainda
+    assim, poderão ser punidos por desonestidade acadêmica.
+
+    Abaixo descreva qualquer ajuda que você recebeu para fazer este
+    EP.  Inclua qualquer ajuda recebida por pessoas (inclusive
+    monitores e colegas). Com exceção de material de MAC0110, caso
+    você tenha utilizado alguma informação, trecho de código,...
+    indique esse fato abaixo para que o seu programa não seja
+    considerado plágio ou irregular.
+
+    Exemplo:
+
+        A monitora me explicou que eu devia utilizar a função int() quando
+        fazemos leitura de números inteiros.
+
+        A minha função quicksort() foi baseada na descrição encontrada na 
+        página https://www.ime.usp.br/~pf/algoritmos/aulas/quick.html.
+
+    Descrição de ajuda ou indicação de fonte:
+
+'''
+
+# escreva seu programa a seguir
+
+s = str(input('Digite uma string s: '))
+i = int(input('Digite um inteiro i: '))
+r = float(input('Digite um real r: '))
+print('s+s = ' + f'{s+s}')
+print('i+i = ' + f'{i+i}')
+print('r+r = ' + f'{r+r}')
+print('i*s = ' + f'{i*s}')
+print('i*i = ' + f'{i*i}')
+print('i*r = ' + f'{i*r}')
+print('r/i = ' + f'{r/i}')
+print('2*i/i = ' + f'{2*i/i}')
+print('i/i*2 = ' + f'{i/i*2}')
